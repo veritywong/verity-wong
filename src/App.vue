dcaf3fa732f23597eceba24c8744a030a1b8dc64
veritywong <veritywong90@gmail.com>
2023-09-22 15:57:13 +0100

    price ordering and nike working together

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,14 @@
           v-model="isNike"
         />
       </div>
+      <div>
+        <label for="priceOrder">Sort By Price </label>
+        <select v-model="priceOrder">
+          <option value=""></option>
+          <option value="ascending">Low - High</option>
+          <option value="descending">High - Low</option>
+        </select>
+      </div>
     </div>
     <p>Results: {{ itemsToDisplay.length }}</p>
 
@@ -55,6 +63,7 @@ export default {
       products: products,
       isAvailable: false,
       isNike: false,
+      priceOrder: '',
     };
   },
   methods: {
@@ -74,12 +83,31 @@ export default {
         ? this.products.filter((item) => item.brand === 'Nike')
         : products;
     },
+    filterByPrice(products) {
+      if (this.priceOrder === "ascending") {
+         return [...products].sort((a, b) => a.price - b.price);
+      } else if (this.priceOrder === "descending") {
+         return [...products].sort((a, b) => b.price - a.price);
+      } else {
+         return products;
+      }
+    },
   },
   computed: {
     itemsToDisplay() {
       let products = this.products;
       let availableProducts = this.filterIsAvailable(products);
+      let productsByPrice = this.filterByPrice(products)
+
+      if (this.isNike || this.priceOrder) {
+        return this.filterByPrice(this.filterNike(products));
+      }
+
+      // in stock and nike filter working togeter
       return this.filterNike(products).filter((product) => availableProducts.includes(product));
+      
+      // price filter working individually
+      // return this.filterByPrice(products)
     },
   },
 };
